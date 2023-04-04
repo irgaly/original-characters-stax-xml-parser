7ffc01def72a014df02e07a39b8eb2aaa13e4237
irgaly <irgaly@gmail.com>
2023-04-04 22:46:37 +0900

    refactor: use version catalog for library

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -12,9 +12,9 @@ tasks.withType<Test> {
 }
 
 dependencies {
-    api("com.fasterxml.woodstox:woodstox-core:6.2.8")
-    testImplementation("io.kotest:kotest-runner-junit5:5.2.2")
-    testImplementation("io.kotest:kotest-assertions-core:5.2.2")
+    api(libs.fasterxml.woodstox)
+    testImplementation(libs.test.kotest.runner)
+    testImplementation(libs.test.kotest.assertions)
 }
 
 java {
